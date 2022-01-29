ef88fbc06508d500308395f2771029d1cd6b1614
X932 <yusuf_020202@mail.ru>
2022-01-29 12:28:22 +0500

    deleted user controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { IUser } from './auth.model';
 import { AuthRoutes } from './auth.routes';
+import { AuthService } from './auth.service';
 
 @Controller(AuthRoutes.Main)
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post()
+  signUp(@Body() user: IUser) {
+    return this.authService.signUp(user);
+  }
+
+  @Get()
+  get() {
+    return this.authService.getUsers();
+  }
+}
