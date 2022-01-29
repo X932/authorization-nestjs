81299170ba9461aec3ab82be95cd9b6a50e54a29
X932 <yusuf_020202@mail.ru>
2022-01-29 17:03:26 +0500

    deleted custom pipe validator, added built-in pipe

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,13 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthenticationDto } from './dto/auth.dto';
 import { AuthRoutes } from './auth.routes';
 import { AuthService } from './auth.service';
-import { ValidatePipe } from '../shared/pipes/validations/validate.pipe';
 
 @Controller(AuthRoutes.Main)
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post(AuthRoutes.SignUp)
-  async signUp(@Body(new ValidatePipe()) user: AuthenticationDto) {
+  async signUp(@Body() user: AuthenticationDto) {
     this.authService.signUp(user);
   }
 
